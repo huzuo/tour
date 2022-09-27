9bd5772d6b95862335024ac6aac4a064a2d61a52
zack.zuo <1146843917@qq.com>
2022-09-27 22:20:43 +0800

    init

---
+++ cmd/root.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+//放置根命令
+var rootCmd = &cobra.Command{}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.AddCommand(wordCmd)
+}
